215b8af90a37541b8d5d54e5ae9f1a8a37dbef0e
Matt Oliver <protogonoi@gmail.com>
2015-11-13 21:24:06 +1100

    project: Update for 1.5 merge.

--- SMP/vpx_config.c
+++ SMP/vpx_config.c
@@ -5,5 +5,6 @@
 /* tree. An additional intellectual property rights grant can be found */
 /* in the file PATENTS.  All contributing project authors may */
 /* be found in the AUTHORS file in the root of the source tree. */
-static const char* const cfg = "--target=x86-win32-vs12 --disable-unit-tests --disable-docs --disable-install-bins --disable-shared --enable-static --enable-vp8 --enable-vp9 --enable-static-msvcrt --enable-vp9-highbitdepth";
+#include "vpx/vpx_codec.h"
+static const char* const cfg = "--target=x86-win32-vs14 --disable-unit-tests --disable-docs --disable-install-bins --disable-shared --enable-static --enable-vp8 --enable-vp9 --enable-static-msvcrt --enable-vp9-highbitdepth";
 const char *vpx_codec_build_config(void) {return cfg;}
